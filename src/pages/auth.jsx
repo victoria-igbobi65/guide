949b52da54c41f0d7e1764b32d0dadf0589db5d9
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-04 00:11:39 +0100

    new: auth pages

---
+++ src/pages/auth.jsx
@@ -0,0 +1,14 @@
+//import React from "react";
+import SignUpPage from "../components/auth/signup";
+import LoginPage from "../components/auth/login";
+
+const Auth = () => {
+    return (
+        <div>
+            <SignUpPage/>
+            <LoginPage/>
+        </div>
+    )
+}
+
+export default Auth;
